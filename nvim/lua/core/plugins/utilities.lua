b65455a48c3f945f2c5b13f7a853bfecc87d485d
Tim Teichmann <teichmanntim@outlook.de>
2024-06-11 21:37:26 +0200

    add vimtex for a better latex experience

--- nvim/lua/core/plugins/utilities.lua
+++ nvim/lua/core/plugins/utilities.lua
@@ -17,6 +17,22 @@ return {
             vim.o.timeoutlen = 300
         end,
         opts = {}
+    },
+    {
+        "lervag/vimtex",
+        lazy = false,
+        config = function()
+            vim.g.vimtex_compiler_latexmk = {
+                out_dir = "build",
+                options = {
+                    "-shell-escape",
+                    "-verbose",
+                    "-file-line-error",
+                    "-interaction=nonstopmode",
+                    "-synctex=1"
+                }
+            }
+        end
     }
 }
 
