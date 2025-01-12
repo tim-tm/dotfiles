ed83b3f952bf1f68e0b89eaa62c2203e00b4ca04
Tim Teichmann <teichmanntim@outlook.de>
2025-01-12 19:54:59 +0100

    fix: bigger polybar icons, termguicolors

--- nvim/lua/core/plugins/theme.lua
+++ nvim/lua/core/plugins/theme.lua
@@ -18,6 +18,7 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            vim.opt.termguicolors = true
             vim.cmd.colorscheme "melange"
         end
     }
