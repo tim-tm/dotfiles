91b554e9564b55cba41c3d968c3b4b1a394e5e2e
Tim Teichmann <teichmanntim@outlook.de>
2025-06-18 23:27:34 +0200

    fix(rofi/scripts): improve passrun and quit

--- nvim/lua/core/plugins/formatting.lua
+++ nvim/lua/core/plugins/formatting.lua
@@ -1,28 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		conform.setup({
-			formatters_by_ft = {
-				typescript = { "prettier" },
-				lua = { "stylua" },
-				python = { "black" },
-				c = { "clang-format" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+        conform.setup({
+            formatters_by_ft = {
+                typescript = { "prettier" },
+                lua = { "stylua" },
+                python = { "black" },
+                c = { "clang-format" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            },
+        })
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end,
+        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
 }
