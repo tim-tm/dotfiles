70df7d52ac1bc4589dcabde38b5508238ad3ff6a
Tim Teichmann <teichmanntim@outlook.de>
2026-02-24 19:46:51 +0100

    refactor(waybar): custom/swaymode

--- waybar/swaymode.sh
+++ waybar/swaymode.sh
@@ -1,3 +1,22 @@
 #!/usr/bin/env fish
 
-swaymsg -r -t get_binding_state | jq -c -M '{text: .name, class: "swaymode"}'
+# 
+
+jq --null-input -c -M \
+	--argjson modes "$(swaymsg -r -t get_binding_modes)" \
+	--argjson mode "$(swaymsg -r -t get_binding_state)" \
+	--arg bgcolor "#ddc7a1" \
+	--arg bgalpha "40%" \
+	'{
+		text: $mode.name,
+		class: "swaymode",
+		tooltip: $modes
+			| map(
+				if . == $mode.name then
+					"<span background=\"" + $bgcolor + "\" background_alpha=\"" + $bgalpha + "\">"
+					+ .
+					+ "</span>"
+				else
+					.
+				end)
+			| join("\\n")}'
