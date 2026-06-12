8e525be03956120c406e831bff986ab3f0b9bb23
Tim Teichmann <teichmanntim@outlook.de>
2026-06-12 21:40:21 +0200

    fix(sway): set wallpaper before executing sway

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,12 @@
 if status is-interactive
-	if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ];
-		read -l -P 'Start sway? [Y/n]' confirm
-		if [ "$confirm" != "N" ] && [ "$confirm" != "n" ];
-			exec sway
-		end
-	end
-	set -gx EDITOR nvim
-	set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-	fish_add_path ~/.nix-profile/bin
+    if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ];
+        read -l -P 'Start sway? [Y/n]' confirm
+        if [ "$confirm" != "N" ] && [ "$confirm" != "n" ];
+            bash -c "DIR='$HOME/.config/sway/wallpaper' $HOME/.config/sway/scripts/randomize_wallpaper"
+            exec sway
+        end
+    end
+    set -gx EDITOR nvim
+    set -gx SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+    fish_add_path ~/.nix-profile/bin
 end
